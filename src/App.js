756baedb38b2bec80cddf0f9b01837a3cab8b6cd
nbalabis <88114907+nbalabis@users.noreply.github.com>
2022-06-25 22:13:24 -0700

    Add colorHelper to generate colors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Palette from "./Palette";
 import seedColors from './seedColors'
+import {generatePalette} from './colorHelpers'
 
 function App() {
   return (
     <div style={{ overflow: "hidden" }}>
       <Palette {...seedColors[4]} />
+      {console.log(generatePalette(seedColors[4]))}
     </div>
   );
 }
