a9c05dd802c305726bd004a1476550b9be488164
nbalabis <88114907+nbalabis@users.noreply.github.com>
2022-06-26 14:43:20 -0700

    Finish styling MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './MiniPalette.css'
 
 function MiniPalette(props) {
-    const { paletteName, emoji } = props
+    const { paletteName, emoji, colors } = props
 
     return (
         <div className='MiniPalette'>
-            <div className='colors'></div>
+            <div className='colors'>
+                {colors.map(color => (
+                    <div
+                        className='mini-color'
+                        style={{ backgroundColor: color.color }}
+                        key={color.name}
+                    />
+                ))}
+            </div>
             <h5 className='title'>
                 {paletteName} <span className='emoji'>{emoji}</span>
             </h5>
