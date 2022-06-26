430c7944e5c9fa254b31caadff63ebbf7b25975f
nbalabis <88114907+nbalabis@users.noreply.github.com>
2022-06-26 15:14:46 -0700

    Add link to ColorBox and add Route

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { Link } from 'react-router-dom';
 import './ColorBox.css'
 
 function ColorBox(props) {
@@ -25,7 +26,9 @@ function ColorBox(props) {
                     </div>
                     <button className='copy-button'>Copy</button>
                 </div>
-                <span className='see-more'>More</span>
+                <Link to='/' onClick={evt => evt.stopPropagation()}>
+                    <span className='see-more'>More</span>
+                </Link>
             </div>
         </CopyToClipboard>
     );
