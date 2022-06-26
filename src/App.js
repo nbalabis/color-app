5d2f2ffc010a0617c061912459d89a2e503d1d9b
nbalabis <88114907+nbalabis@users.noreply.github.com>
2022-06-25 22:29:20 -0700

    Add unstyled color slider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Palette from "./Palette";
 import seedColors from './seedColors'
-import {generatePalette} from './colorHelpers'
+import { generatePalette } from './colorHelpers'
 
 function App() {
   return (
     <div style={{ overflow: "hidden" }}>
-      <Palette {...seedColors[4]} />
-      {console.log(generatePalette(seedColors[4]))}
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
--- src/Palette.js
+++ src/Palette.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
 import ColorBox from './ColorBox';
 import './Palette.css'
 
 function Palette(props) {
-    const colorBoxes = props.colors.map(color => (
-        <ColorBox background={color.color} name={color.name} />
-    ))
+    const [level, setLevel] = useState(500)
 
+    const { colors } = props.palette
+    const colorBoxes = colors[level].map(color => (
+        <ColorBox background={color.hex} name={color.name} />
+    ))
 
     return (
         <div className='Palette'>
+            <Slider
+                defaultValue={level}
+                min={100}
+                max={900}
+                step={100}
+                onChange={(level) => setLevel(level)}
+            />
             {/* Navbar goes here */}
             <div className='Palette-colors'>
                 {colorBoxes}
