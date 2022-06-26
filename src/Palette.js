be7b96af71cb927abdf9e9d73f40bf48ce2a9551
nbalabis <88114907+nbalabis@users.noreply.github.com>
2022-06-25 19:40:15 -0700

    Add palette

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Palette(props) {
+    return (
+        <div className='Palette'>
+            {/* Navbar goes here */}
+            <div className='Palette-colors'>
+                {/* Bunch of colors here */}
+            </div>
+            {/* Footer goes here */}
+        </div>
+    );
+}
+
+export default Palette;
